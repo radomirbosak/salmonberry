072022d94d982ef5cb80e037fc2e7ed32cbac655
Radomír Bosák <radomir.bosak@gmail.com>
2018-05-08 08:45:24 +0200

    Add feature extraction and prediction

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 Machine learning powered RSS feed filtering
 
+![salmonberry screenshot](screenshot.png)
+
 Inspiration: https://blog.algorithmia.com/create-your-own-machine-learning-powered-rss/
--- salmonberry.py
+++ salmonberry.py
@@ -3,12 +3,16 @@ from collections import Counter
 
 import yaml
 import feedparser
-
+import scipy
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.linear_model import LinearRegression
 
 logging.basicConfig(level=logging.DEBUG)
 
 
-url = 'https://www.reddit.com/r/MachineLearning/.rss'
+
+url = 'https://www.reddit.com/r/MachineLearning/new/.rss'
+# url = 'https://www.reddit.com/r/artificial/new/.rss'
 RATING_FILENAME = 'ratings.yaml'
 
 
@@ -19,14 +23,34 @@ def main():
 
     answers = get_answers(RATING_FILENAME)
 
-    new_entries = {entry for entry in feed.entries if entry.id not in answers}
-    print(f'Found {len(new_entries)} new entries')
+    if answers:
+        vectorizer = get_vectorizer(answers.values())
+        vectors = get_vectors(answers.values(), vectorizer)
+
+        transformer = TfidfTransformer()
+        features = transformer.fit_transform(vectors)
+
+        linreg = LinearRegression()
+        targets = scipy.array([1 if ans['rating'] == 'y' else 0 for ans in answers.values()])
+        # import ipdb; ipdb.set_trace()
+        linreg.fit(features, targets)
+
+    new_entries = {entry for entry in feed.entries if entry.link not in answers}
+    print(f'Found {len(new_entries)} new entries, {len(answers)} old entries.')
 
     new_answers = {}
     print('Are these titles interesting? [y/n]')
     for i, entry in enumerate(new_entries, start=1):
 
         prompt = f'{i}/{len(new_entries)} | {entry.title}: [y/n] '
+
+        if answers:
+            new_vect = vectorizer.transform([entry.title])
+            new_vect2 = transformer.transform(new_vect)
+            hod = linreg.predict(new_vect2)[0]
+
+            percent = int(round(hod * 100))
+            print(f'Predicted value: {percent}%')
 
         try:
             answer = input(prompt)
@@ -35,15 +59,30 @@ def main():
             logging.debug('Exiting')
             break
 
-        new_answers[entry.id] = answer
+        entrydict = dict(entry)
+        entrydict['rating'] = answer
 
-    c = Counter(new_answers.values())
+        new_answers[entry.link] = entrydict
+
+    c = Counter([ans['rating'] for ans in new_answers.values()])
 
     others = len(new_answers) - c["y"] - c["n"]
-    print(f'yes: {c["y"]}, no: {c["n"]}, others: {others}')
+    print(f'Answers | yes: {c["y"]}, no: {c["n"]}, others: {others}')
 
     answers.update(new_answers)
     dump_answers(answers, RATING_FILENAME)
+
+
+def get_vectorizer(entries):
+    titles = [doc["title"] for doc in entries]
+    vect = CountVectorizer()
+    vect.fit(titles)
+    return vect
+
+
+def get_vectors(entries, vectorizer):
+    titles = [doc["title"] for doc in entries]
+    return vectorizer.transform(titles)
 
 
 def dump_answers(answers, filename):
