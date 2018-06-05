70913b725e2e75fad34d5726d391d52dd0b7baa0
Radomír Bosák <radomir.bosak@gmail.com>
2018-06-05 09:21:02 +0200

    Remove unused code

--- salmonberry/cli.py
+++ salmonberry/cli.py
@@ -20,70 +20,6 @@ CACHE_FILENAME = 'data/cache.yaml'
 LABELS_FILENAME = 'data/labels.yaml'
 
 
-class Predictor:
-
-    def __init__(self, entries):
-        self.entries = entries
-
-        if not self.entries:
-            return
-
-        self.vectorizer = get_vectorizer(entries)
-        vectors = get_vectors(entries, self.vectorizer)
-
-        self.transformer = TfidfTransformer()
-        self.features = self.transformer.fit_transform(vectors)
-
-        self.linreg = LinearRegression()
-        self.targets = scipy.array([1 if ans['rating'] == 'y'
-                                    else 0 for ans in entries])
-        self.linreg.fit(self.features, self.targets)
-
-    def predict(self, new_title):
-        if not self.entries:
-            return None
-
-        new_vect = self.vectorizer.transform([new_title])
-        new_vect2 = self.transformer.transform(new_vect)
-        return self.linreg.predict(new_vect2)[0]
-
-    @property
-    def error(self):
-        # -- 1.
-        errors = []
-        k_fold = 10
-
-        for _ in range(k_fold):
-            train_feat, test_feat, train_tar, test_tar = \
-                train_test_split(self.features, self.targets)
-
-            test_lin_reg = LinearRegression()
-            test_lin_reg.fit(train_feat, train_tar)
-
-            test_pred = test_lin_reg.predict(test_feat)
-
-            absvals = np.abs(test_tar - test_pred)
-            errors.append(absvals.mean())
-
-        return np.average(errors), np.std(errors)
-
-
-def percent(value):
-    return int(round(value * 100))
-
-
-def get_vectorizer(entries):
-    titles = [doc["title"] for doc in entries]
-    vect = CountVectorizer()
-    vect.fit(titles)
-    return vect
-
-
-def get_vectors(entries, vectorizer):
-    titles = [doc["title"] for doc in entries]
-    return vectorizer.transform(titles)
-
-
 def get_feed_urls(filename):
     with open(FEEDS_FILENAME) as fd:
         return yaml.load(fd)
@@ -99,61 +35,6 @@ def get_all_entries(feed_urls):
     logging.debug('Downloaded %d entries from %d feeds.', len(entries), len(feed_urls))
 
     return entries
-
-
-def save_ratings(answers, filename):
-    with open(filename, 'w') as fd:
-        yaml.dump(answers, fd)
-
-
-def load_ratings(filename):
-    try:
-        with open(filename, 'r') as fd:
-            return yaml.load(fd)
-    except FileNotFoundError:
-        return {}
-
-
-def learn():
-    urls = get_feed_urls(FEEDS_FILENAME)
-    fetched_entries = get_all_entries(urls)
-
-    answers = load_ratings(RATING_FILENAME)
-
-    predictor = Predictor(answers.values())
-
-    new_entries = {entry for entry in fetched_entries if entry.link not in answers}
-    print(f'Found {len(new_entries)} new entries, {len(answers)} old entries.')
-
-    new_answers = {}
-    print('Are these titles interesting? [y/n]')
-    for i, entry in enumerate(new_entries, start=1):
-
-        prompt = f'{i}/{len(new_entries)} | {entry.title}: [y/n] '
-
-        value = predictor.predict(entry.title)
-        if value is not None:
-            print(f'Predicted value: {percent(value)}%')
-
-        try:
-            answer = input(prompt)
-        except EOFError:
-            print()
-            logging.debug('Exiting')
-            break
-
-        entrydict = dict(entry)
-        entrydict['rating'] = answer
-
-        new_answers[entry.link] = entrydict
-
-    c = Counter([ans['rating'] for ans in new_answers.values()])
-
-    others = len(new_answers) - c["y"] - c["n"]
-    print(f'Answers | yes: {c["y"]}, no: {c["n"]}, others: {others}')
-
-    answers.update(new_answers)
-    save_ratings(answers, RATING_FILENAME)
 
 
 def to_dicts(tree):
